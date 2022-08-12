989ca49b5474d47bd27118f3a9bbfb9f4aabe848
daidr <daidr@daidr.me>
2022-08-12 20:16:05 +0800

    chore: add comment

--- config.example.js
+++ config.example.js
@@ -1,4 +1,10 @@
 module.exports = {
+    /** 
+     * domains  用于配置需要被检测的域名 
+     *     name     用于配置名称，会显示在结果中
+     *     source   用于配置域名的源，可以是一个字符串，也可以是一个 URL 对象
+     *     summary  (可选) 用于配置结果中的摘要，会显示在结果中
+    */
     "domains": [
         {
             "name": "戴兜的小屋",
@@ -19,6 +25,11 @@ module.exports = {
             "source": new URL("https://sentry.daidr.me"),
         },
     ],
+    /**
+     * footer 用于配置页脚信息
+     *     copyright   用于配置页脚的版权信息
+     *     links       用于配置页脚的链接信息
+     */
     footer: {
         copyright: '© 戴兜的小屋',
         links: [
@@ -34,6 +45,14 @@ module.exports = {
             }
         ]
     },
+    /**
+     * scopeVariable 用于配置模板中的自定义变量
+     * 
+     * 此处的变量在映射到模板变量时，会添加 “EX” 前缀
+     * 例如下方预定义的变量 pageTitle 将会映射到模板变量 EXpageTitle
+     * 
+     * 你可以打开 template.html 来查看模板变量的定义
+     */
     scopeVariable: {
         pageTitle: 'DaiDR Cert Monitor',
     }
